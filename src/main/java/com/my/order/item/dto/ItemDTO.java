b51586c343d5b80f16dddef1d3c075fb2d5a669f
jisung324 <jisung3245@gmail.com>
2022-06-01 22:56:36 +0900

    itemDTO List 추가

--- src/main/java/com/my/order/item/dto/ItemDTO.java
+++ src/main/java/com/my/order/item/dto/ItemDTO.java
@@ -1,4 +1,23 @@
 package com.my.order.item.dto;
+import java.util.List;
+import lombok.Data;
 
+@Data
 public class ItemDTO {
+
+    private Long itemNo;
+
+    private String itemTitle;
+
+    private String itemPrice;
+
+    private String itemCategory;
+
+    private int ItemStock;
+
+    public List<ItemDTO> ItemList;
+
+    public List<ItemDTO> getItemList(){
+        return ItemList;
+    }
 }
